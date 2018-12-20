b8fc6f737f98d46d8ee86ce825111f56ecb758c5
kuba.natonek998 <kuba.natonek998@gmail.com>
2018-12-20 13:53:35 +0100

    lab 8 -> login and unlogin protocol

---
+++ lab8 - c#/lab9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpServer server = new TcpServer();
+            server.Run();
+
+            while (true)
+            {
+                System.Threading.Thread.Sleep(100);
+                Console.Write("\n Your request:");
+                TcpClientHandler.ClientRequestString = Console.ReadLine();
+                TcpClientHandler.ConnectAsTcpClient("127.0.0.1", 1234);
+            }
+        }
+              
+    }
+}
